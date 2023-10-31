8ae686a7de937b0c0c1095e2359b407905b1e9aa
Omar Bernal <omarbernaljs@gmail.com>
2023-10-31 17:15:37 -0500

    add: Layout component  to components folder

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import TransactionItem from "./TransactionItem";
+
+
+type LayoutProps = {
+    children?: React.ReactNode;
+  }
+  export default function Layout({ children }: LayoutProps) {
+    return (
+      <>
+        <Header/>
+        {children}
+      </>
+    )
+  }
