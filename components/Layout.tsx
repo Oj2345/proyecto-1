dd1c62450c8501f889ab18353fb946e0ccba3872
Omar Bernal <omarbernaljs@gmail.com>
2023-11-02 10:16:42 -0500

    feat: import Head of index to Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,28 @@
+import Head from "next/head";
 import Header from "./Header";
-import TransactionItem from "./TransactionItem";
 
 
- type LayoutProps = {
-    children?: React.ReactNode;
-  }
-  export default function Layout({ children }: LayoutProps) {
-    return (
-      <>
-        <Header/>
-        {children}
-      </>
-    )
-  }
+type LayoutProps = {
+  children?: React.ReactNode;
+  title?: string;
+};
+export default function Layout({ children, ...customMeta }: LayoutProps) {
+  const meta = {
+    title: "proyecto-test",
+    ...customMeta,
+  };
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=
+        , initial-scale=1.0"
+        />
+        <title>{meta.title}</title>
+      </Head>
+      <Header />
+      {children}
+    </>
+  );
+}
