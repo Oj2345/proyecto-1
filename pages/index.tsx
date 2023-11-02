58b9f71e226c21e804e5d915554296b2db2a236d
Omar Bernal <omarbernaljs@gmail.com>
2023-11-02 10:27:28 -0500

    refactor: remove Head from index and add it to Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { MonitorSmartphone, ShoppingCart } from "lucide-react";
-import Head from "next/head";
 import { Poppins } from "next/font/google";
-import TransactionItem from "@/components/TransactionItem";
+import Transaction from "@/components/Transaction";
 import Layout from "@/components/Layout";
 
 const poppins = Poppins({
@@ -10,39 +9,30 @@ const poppins = Poppins({
 });
 export default function HomePage() {
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=
-        , initial-scale=1.0"
-        />
-        <title>Proyecto-Test</title>
-      </Head>
-        <Layout/>
+    <Layout>
       <main className={poppins.className}>
-      <TransactionItem
-        icon= {<ShoppingCart />}
-        title="Online Store"
-        date="Jul 7,2023 at 11:55"
-        price={60.00}
-        status="success"
+        <Transaction
+          icon={<ShoppingCart />}
+          title="Online Store"
+          date="Jul 7,2023 at 11:55"
+          price={60.0}
+          status="success"
         />
-        <TransactionItem
-        icon= {<MonitorSmartphone />}
-        title="Merchant"
-        date="Jul 7,2023 at 11:55"
-        price={100.00}
-        status="pending"
+        <Transaction
+          icon={<MonitorSmartphone />}
+          title="Merchant"
+          date="Jul 7,2023 at 11:55"
+          price={100.0}
+          status="pending"
         />
-        <TransactionItem
-        icon= {<ShoppingCart  />}
-        title="Online Store"
-        date="Jul 7,2023 at 11:55"
-        price={23.00}
-        status="expired"
+        <Transaction
+          icon={<ShoppingCart />}
+          title="Online Store"
+          date="Jul 7,2023 at 11:55"
+          price={23.0}
+          status="expired"
         />
       </main>
-    </>
+    </Layout>
   );
 }
