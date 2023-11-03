ca11bc0e3929ab40fb8159e877ffa128da6eedeb
Omar Bernal <omarbernaljs@gmail.com>
2023-11-03 16:54:32 -0500

    refactor: rename class Title to title

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,13 +11,13 @@ export default function Header() {
   return (
     <header className={`${style.content} ${poppins.className}`}>
       <div className={style.text1}>
-        <p className={style.Title}>Zapay</p>
+        <p className={style.title}>Zapay</p>
         <a href="#" className={style.perfil}>
           Perfil
         </a>
       </div>
       <div className={style.text2}>
-        <p className={style.Title}>Lasted payments</p>
+        <p className={style.title}>Lasted payments</p>
         <a href="#" className={style.viewAll}>
           viewAll
         </a>
