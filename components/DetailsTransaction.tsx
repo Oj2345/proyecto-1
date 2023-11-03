b3a4d672b7ca6e0f5235ff910d15f376de6e3e4a
Omar Bernal <omarbernaljs@gmail.com>
2023-11-03 16:26:54 -0500

    feat: new component DetailsTransaction

---
+++ components/DetailsTransaction.tsx
@@ -0,0 +1,24 @@
+import style from "@/styles/DetailsTransaction.module.css";
+
+
+export interface DetailsTransactionProps {
+  icon: React.ReactNode;
+  title: string;
+  date: string;
+  price: number;
+}
+export default function Transaction({
+  icon,
+  title,
+  date,
+  price,
+}: DetailsTransactionProps) {
+  return (
+    <header className={style.content}>
+      <div className={style.text}>
+        <p className={style.title}>Zapay</p>
+        <a href="#" className={style.perfil}>Perfil</a>
+      </div>
+    </header>
+  );
+}
