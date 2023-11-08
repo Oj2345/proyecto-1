aaf385cf57be6a6604cf142b14e942f89bddedb9
Omar Bernal <omarbernaljs@gmail.com>
2023-11-08 09:39:32 -0500

    feat: import component paymentType to transaction page

--- pages/transaction/[transaction].tsx
+++ pages/transaction/[transaction].tsx
@@ -1,6 +1,8 @@
-import DetailsTransaction from "@/components/DetailsTransaction";
-
+import { Copy, ShoppingCart } from "lucide-react";
+import Layout from "@/components/Layout";
 import { Poppins } from "next/font/google";
+import PaymentType from "@/components/PaymentType";
+import TransactionDetails from "@/components/TransactionDetails";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -8,9 +10,22 @@ const poppins = Poppins({
 });
 export default function transaction() {
   return (
-    <main className={poppins.className}>
-      <DetailsTransaction/>
-    </main>
-
+    <Layout>
+       <main className={poppins.className}>
+        <PaymentType
+          icon={<ShoppingCart />}
+          title="Online shop"
+          price="$60.00"
+        />
+        <TransactionDetails
+        title="Transaction Details"
+        hash="0x08f82194...edc42f14c06ed4ff"
+        icon={<Copy />}
+        adress="0xbe80...d62c"
+        status="success"
+        date= "Jul 7, 2023 at 11:55"
+        />
+      </main>
+    </Layout>
   );
 }
