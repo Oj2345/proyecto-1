13c689e07bd78e6f2eccc6e7ed07e32984fe08c7
Omar Bernal <omarbernaljs@gmail.com>
2023-11-14 08:42:20 -0500

    refactor: change name function ChevronToggle to useToggle

--- components/TransactionDetails.tsx
+++ components/TransactionDetails.tsx
@@ -1,5 +1,6 @@
 import style from "@/styles/TransactionDetails.module.css";
-import { ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { Fragment, useState } from "react";
 
 export interface TransactionDetailsProps {
   title: string;
@@ -17,42 +18,60 @@ export default function TransactionDetails({
   status,
   date,
 }: TransactionDetailsProps) {
+  const { toggle, onChangeToggle } = useToggle();
   return (
     <section className={style.content}>
       <div className={style.contentText}>
         <div className={style.contentText2}>
           <article className={style.article}>
             <p className={style.title}>{title}</p>
-            <button className={style.button}>
-              <ChevronUp />
+            <button onClick={onChangeToggle} className={style.button}>
+              <ChevronDown />
             </button>
           </article>
-          <article className={style.article}>
-            <p className={style.text}>Transaction hash</p>
-            <div className={style.iconFlex}>
-              <p className={style.hash}>{hash}</p>
-              <div className={style.icon}>{icon}</div>
-            </div>
-          </article>
-          <article className={style.article}>
-            <p className={style.text}>Adress</p>
-            <div className={style.iconFlex}>
-              <p className={style.adress}>{adress}</p>
-              <a href="#" className={style.icon}>
-                {icon}
-              </a>
-            </div>
-          </article>
-          <article className={style.article}>
-            <p className={style.text}>Payment status</p>
-            <p className={`${style.Status} ${style[status]}`}>{status}</p>
-          </article>
-          <article className={style.article}>
-            <p className={style.text}>Payment time</p>
-            <p className={style.date}>{date}</p>
-          </article>
+          {toggle && (
+            <Fragment>
+              <section className={style.contenArticle}>
+                <article className={style.article}>
+                  <p className={style.text}>Transaction hash</p>
+                  <div className={style.iconFlex}>
+                    <p className={style.hash}>{hash}</p>
+                    <div className={style.icon}>{icon}</div>
+                  </div>
+                </article>
+                <article className={style.article}>
+                  <p className={style.text}>Adress</p>
+                  <div className={style.iconFlex}>
+                    <p className={style.adress}>{adress}</p>
+                    <a href="#" className={style.icon}>
+                      {icon}
+                    </a>
+                  </div>
+                </article>
+                <article className={style.article}>
+                  <p className={style.text}>Payment status</p>
+                  <p className={`${style.Status} ${style[status]}`}>{status}</p>
+                </article>
+                <article className={style.article}>
+                  <p className={style.text}>Payment time</p>
+                  <p className={style.date}>{date}</p>
+                </article>
+              </section>
+            </Fragment>
+          )}
         </div>
       </div>
     </section>
   );
+}
+
+export function useToggle() {
+  const [toggle, setToggle] = useState(false);
+  const onChangeToggle = () => {
+    setToggle(!toggle);
+  };
+  return {
+    toggle,
+    onChangeToggle,
+  };
 }
