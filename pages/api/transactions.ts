a81dfef90a121b8e96a3e9f840630382493f904a
Jair Pérez <oquendojair10@gmail.com>
2023-11-20 18:26:55 -0500

    feat: add transaction api

---
+++ pages/api/transactions.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Transactions = {
+  id: number;
+  date: string;
+  price: number;
+  status: "success" | "pending" | "expired";
+  type?: "online-store" | "merchant";
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Transactions[]>
+) {
+  res.status(200).json([
+    {
+      id: 1,
+      type: "online-store",
+      date: "Jul 7,2023 at 11:55",
+      price: 60.0,
+      status: "success",
+    },
+    {
+      id: 2,
+      type: "merchant",
+      price: 100.0,
+      date: "Jul 7,2023 at 11:55",
+      status: "pending",
+    },
+    {
+      id: 3,
+      type: "online-store",
+      price: 23.0,
+      date: "Jul 7,2023 at 11:55",
+      status: "expired",
+    },
+  ]);
+}
